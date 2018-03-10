37aede599313fba26921b0eb6c780724273f998a
Juan P. Dantur <jpdantur@gmail.com>
2018-03-10 17:45:52 -0300

    Incomplete Main

--- src/main/java/ar/edu/itba/ss/App.java
+++ src/main/java/ar/edu/itba/ss/App.java
@@ -1,13 +1,26 @@
 package ar.edu.itba.ss;
+import org.apache.commons.math3.random.*;
+import org.apache.commons.math3.util.FastMath;
+import java.util.*;
 
-/**
- * Hello world!
- *
- */
+
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static final int N = 1000;
+    public static final int L = 100;
+    public static final double SPEED = 0.03;
+    public static void main( String[] args ) {
+        RandomDataGenerator rng = new RandomDataGenerator();
+        List<Particle> particleList = new ArrayList<Particle>(N);
+        for (int i=0; i<N;i++) {
+            particleList.add(new Particle(rng.nextUniform(0,L,true),
+                    rng.nextUniform(0,L,true),
+                    SPEED,
+                    rng.nextUniform(0,2*FastMath.PI,true)));
+        }
+
+        //TODO: Actualizar con el tiempo y escupir en un archivo
+
     }
 }
