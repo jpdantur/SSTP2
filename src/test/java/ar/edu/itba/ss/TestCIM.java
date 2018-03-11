9d0a8cf6b0165d55147357797dce2fb6f3f07d05
scamisay <scamisay@candoit.com.ar>
2018-03-10 23:48:38 -0300

    -agrego Cell Index Method -TODO: adaptar Particle al CIM o usar domain.Particle en este TP.

---
+++ src/test/java/ar/edu/itba/ss/TestCIM.java
@@ -0,0 +1,43 @@
+import ar.edu.itba.ss.domain.Particle;
+import ar.edu.itba.ss.helper.ParticleGenerator;
+import ar.edu.itba.ss.helper.Printer;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class TestCIM {
+    private static final Double RADIX = .1;
+
+    public static void main(String[] args) {
+        Integer N, M;
+        Double L, rc;
+        boolean periodicContourCondition;
+        while (true) {
+            try {
+                Scanner sc = new Scanner(System.in);
+                System.out.print("N:");
+                N = sc.nextInt();
+
+                System.out.print("L:");
+                L = sc.nextDouble();
+
+                System.out.print("M:");
+                M = sc.nextInt();
+
+                System.out.print("rc:");
+                rc = sc.nextDouble();
+
+                System.out.print("periodicContourCondition:");
+                periodicContourCondition = sc.nextBoolean();
+                break;
+            } catch (Exception e) {
+                System.out.println("Vuelva a intentar introducir los valores");
+            }
+        }
+
+        List<Particle> particles = new ParticleGenerator().generate(N, L, RADIX);
+        Printer printer = new Printer(particles, L, M, rc, periodicContourCondition, 0);
+        printer.printFiles();
+
+    }
+}
