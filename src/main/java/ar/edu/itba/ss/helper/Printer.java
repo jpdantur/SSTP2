c42b785581a4ad44d8e3ebc25e7892e5dd247e81
Juan P. Dantur <jpdantur@gmail.com>
2018-03-13 15:54:21 -0300

    More Refactor

--- src/main/java/ar/edu/itba/ss/helper/Printer.java
+++ src/main/java/ar/edu/itba/ss/helper/Printer.java
@@ -29,9 +29,9 @@ public class Printer {
     private static final String FILE_NAME_NEIGHBOURS = "neigbours.txt";
     private static final String FILE_NAME_OVITO = "ovito.xyz";
 
-    private static List<Double> COLOR_OUT_OF_SELECTION = Arrays.asList(0.63922,   0.76078,   0.76078);
-    private static List<Double> COLOR_NEIGBOURH = Arrays.asList(1.00000,   0.30196,   0.58039);
-    private static List<Double> COLOR_SELECTED = Arrays.asList(1.00000,   0.80000,   0.00000);
+    private static final List<Double> COLOR_OUT_OF_SELECTION = Arrays.asList(0.63922,   0.76078,   0.76078);
+    private static final List<Double> COLOR_NEIGBOURH = Arrays.asList(1.00000,   0.30196,   0.58039);
+    private static final List<Double> COLOR_SELECTED = Arrays.asList(1.00000,   0.80000,   0.00000);
 
     private Map<Particle, List<Particle>> calculated;
     private Map<Particle, List<Double>> particlesColors = new HashMap<>();
@@ -78,27 +78,31 @@ public class Printer {
 
     private void printStringToFile(String filename, String content){
         try {
-            Files.write(Paths.get(filename), content.toString().getBytes());
+            Files.write(Paths.get(filename), content.getBytes());
         } catch (IOException e) {
             System.out.println("No se pudo crear el archivo "+filename);
         }
     }
 
     private void printForOvito() {
-        printStringToFile(FILE_NAME_OVITO, printParticles().toString());
+        printStringToFile(FILE_NAME_OVITO, printParticles());
     }
 
-    public String printParticles() {
-        StringBuffer sb = new StringBuffer();
-        sb.append(particles.size()+"\n");
-        sb.append(particles.size()+"\n");
+    private String printParticles() {
+        return particles.size() + "\n" +
+                particles.size() + "\n" +
+                particles.stream()
+                        .map(p -> String.format("%.6f %.6f %.6f %.5f %.5f %.5f",
+                                p.getX(),p.getY(), p.getRadix(), getRed(p), getGeen(p), getBlue(p)))
+                        .collect(Collectors.joining("\n")).replaceAll(",","\\.");
+/*       sb.append(particles.size()).append("\n");
+        sb.append(particles.size()).append("\n");
         sb.append(
                 particles.stream()
                         .map(p -> String.format("%.6f %.6f %.6f %.5f %.5f %.5f",
                                 p.getX(),p.getY(), p.getRadix(), getRed(p), getGeen(p), getBlue(p)))
                         .collect(Collectors.joining("\n")).replaceAll(",","\\.")
-        );
-        return sb.toString();
+        ); */
     }
 
     private double getBlue(Particle p) {
@@ -115,12 +119,12 @@ public class Printer {
 
 
     private void printNeighbours() {
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         calculated.keySet().forEach(
             k -> sb.append(String.format(
-                    "%s\n%s\n\n",
+                    "%s%n%s%n%n",
                     k.printParticle(),
-                    calculated.get(k).stream().map(p -> p.printParticle()).collect(joining(", "))
+                    calculated.get(k).stream().map(Particle::printParticle).collect(joining(", "))
                     ))
         );
 
