b66a6a9d81ceddbaa00ca79f9f2c0638c6df96fe
Juan P. Dantur <jpdantur@gmail.com>
2018-03-13 13:05:52 -0300

    Change RNG to Apache

--- src/main/java/ar/edu/itba/ss/helper/ParticleGenerator.java
+++ src/main/java/ar/edu/itba/ss/helper/ParticleGenerator.java
@@ -1,9 +1,10 @@
 package ar.edu.itba.ss.helper;
 
 import ar.edu.itba.ss.domain.Particle;
+import org.apache.commons.math3.random.JDKRandomGenerator;
+import org.apache.commons.math3.random.RandomDataGenerator;
 
 import java.util.List;
-import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -13,10 +14,10 @@ import java.util.stream.IntStream;
 public class ParticleGenerator {
 
     public List<Particle> generate(Integer N, Double L, Double radix) {
-        Long seed = 50000L;// new Double(Math.random()*100000).longValue();
-        Random generator = new Random(seed);
+        int seed = 50000;// new Double(Math.random()*100000).longValue();
+        RandomDataGenerator generator = new RandomDataGenerator(new JDKRandomGenerator(seed));
         return IntStream.range(0,N)
-                .mapToObj(i ->new Particle(generator.nextDouble()*L, generator.nextDouble()*L, radix))
+                .mapToObj(i ->new Particle(generator.nextUniform(0,L,true), generator.nextUniform(0,L,true), radix))
                 .collect(Collectors.toList());
     }
 }
