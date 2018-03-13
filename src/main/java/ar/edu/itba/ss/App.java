9fa35b98284527d4a690aee04a372fc70f6dc7cb
Juan P. Dantur <jpdantur@gmail.com>
2018-03-13 13:08:20 -0300

    Remove old Particle Class

--- src/main/java/ar/edu/itba/ss/App.java
+++ src/main/java/ar/edu/itba/ss/App.java
@@ -11,14 +11,6 @@ public class App
     public static final int L = 100;
     public static final double SPEED = 0.03;
     public static void main( String[] args ) {
-        RandomDataGenerator rng = new RandomDataGenerator();
-        List<Particle> particleList = new ArrayList<Particle>(N);
-        for (int i=0; i<N;i++) {
-            particleList.add(new Particle(rng.nextUniform(0,L,true),
-                    rng.nextUniform(0,L,true),
-                    SPEED,
-                    rng.nextUniform(0,2*FastMath.PI,true)));
-        }
 
         //TODO: Actualizar con el tiempo y escupir en un archivo
 
